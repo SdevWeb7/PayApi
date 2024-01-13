173a5e5472d87361554b30bde9dacdb012cc22a0
SdevWeb7 <152064117+SdevWeb7@users.noreply.github.com>
2024-01-13 19:07:11 +0100

    Add files via upload

---
+++ public/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home.jsx";
+import { Pricing } from "./pages/Pricing.jsx";
+import { About } from "./pages/About.jsx";
+import { Contact } from "./pages/Contact.jsx";
+
+function App() {
+
+  return (
+    <>
+       <BrowserRouter basename={'/PayApi'}>
+          <Routes>
+             <Route path={'/'} element={<Home />} />
+             <Route path={'/pricing'} element={<Pricing />} />
+             <Route path={'/about'} element={<About />} />
+             <Route path={'/contact'} element={<Contact />} />
+          </Routes>
+       </BrowserRouter>
+    </>
+  )
+}
+
+export default App
